eb41b7aaeb2b6dfa0088d0f7cd8f9816f807f6ab
lucas.pires <lfpires1993@gmail.com>
2021-06-10 11:30:53 -0300

    Add Packages

--- app/src/main/java/com/lfpires/recyclerviewtests/activity/MainActivity.java
+++ app/src/main/java/com/lfpires/recyclerviewtests/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.lfpires.recyclerviewtests;
+package com.lfpires.recyclerviewtests.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.lfpires.recyclerviewtests.R;
 
 public class MainActivity extends AppCompatActivity {
 
