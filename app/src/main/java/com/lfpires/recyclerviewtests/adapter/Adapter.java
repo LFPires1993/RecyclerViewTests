9bd6925a99ebe443156df0d1b85ad42f4c5363e9
lucas.pires <lfpires1993@gmail.com>
2021-06-10 11:43:58 -0300

    View Holder Created

--- app/src/main/java/com/lfpires/recyclerviewtests/adapter/Adapter.java
+++ app/src/main/java/com/lfpires/recyclerviewtests/adapter/Adapter.java
@@ -1,4 +1,17 @@
 package com.lfpires.recyclerviewtests.adapter;
 
-public class Adapter {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
