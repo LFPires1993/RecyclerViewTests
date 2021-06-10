22663736bd4744985fabd3b522d797b913e8841d
lucas.pires <lfpires1993@gmail.com>
2021-06-10 11:46:58 -0300

    Implementing adapter methods

--- app/src/main/java/com/lfpires/recyclerviewtests/adapter/Adapter.java
+++ app/src/main/java/com/lfpires/recyclerviewtests/adapter/Adapter.java
@@ -1,17 +1,37 @@
 package com.lfpires.recyclerviewtests.adapter;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titutlo, genero, ano;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
         }
     }
-
 }
