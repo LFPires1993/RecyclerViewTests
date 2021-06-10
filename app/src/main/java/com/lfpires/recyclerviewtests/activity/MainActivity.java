8a199dd55f6f4b93266cea441c16895368ff8dc5
lucas.pires <lfpires1993@gmail.com>
2021-06-10 11:32:28 -0300

    Add object

--- app/src/main/java/com/lfpires/recyclerviewtests/activity/MainActivity.java
+++ app/src/main/java/com/lfpires/recyclerviewtests/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lfpires.recyclerviewtests.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,9 +9,14 @@ import com.lfpires.recyclerviewtests.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerview = findViewById(R.id.recyclerView);
+
     }
 }
